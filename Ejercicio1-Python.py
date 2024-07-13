845b7b7f24e25c8fe3f2a50fd7793b3631cb1666
Ander Holden <urbanoivan10@gmail.com>
2024-07-13 16:00:46 -0500

    Mi primer ejercicio en Python

---
+++ Ejercicio1-Python.py
@@ -0,0 +1,12 @@
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Ingrese su nombre: ")
+# Solicitar al usuario que ingrese su edad
+edad = input("Ingresa tu edad: ")
+# Mostrar un mensaje de bienvenida utilizando el nombre y la edad proporcionados
+print(f"Bienvenido, {nombre} de {edad} años.")
+# Solicitar al usuario que ingrese un número
+num1 = input("Ingresa un número: ")
+# Convertir el número ingresado a un tipo de dato entero
+num2 = int(num1)
+# Mostrar el doble de un número ingresado
+print(f"El doble de {num2} es: {num2*2}")
